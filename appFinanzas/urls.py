20d5eee5c0d83fdcd9c4ba768d52051b0d0b6a24
MEOM2001 <workmarcos12@gmail.com>
2025-03-07 12:21:52 -0500

    Primera version

---
+++ appFinanzas/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from myApp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('gastosIngresos/', views.gastosIngresos, name='gastos ingresos'),
+    path('', views.iniciarSesion, name='iniciar sesion'),
+    path('inicio/', views.inicio, name='inicio'),
+    path('presupuestos/', views.presupuestos, name='presupuestos'),
+    path('cerrarSesion/', views.cerrarSesion, name='cerrar sesion'),
+    path('registrarse/', views.registrarse, name='registrarse')
+]
---
+++ myApp/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.db import IntegrityError
+
+
+def gastosIngresos(request):
+    return render(request, 'gastosIngresos.html')
+
+
+def iniciarSesion(request):
+    if request.method == 'GET':
+        return render(request, 'iniciarSesion.html', {
+            'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'iniciarSesion.html', {
+                'form': AuthenticationForm,
+                'error': 'Username or password is incorrect'
+            })
+        else:
+            login(request, user)
+            return redirect('inicio')
+
+
+def inicio(request):
+    return render(request, 'inicio.html')
+
+
+def presupuestos(request):
+    return render(request, 'presupuestos.html')
+
+
+def registrarse(request):
+
+    if request.method == 'GET':
+        return render(request, 'registrarse.html', {
+            'form': UserCreationForm
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(username=request.POST['username'],
+                                                password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('inicio')
+            except IntegrityError:
+                return render(request, 'registrarse.html', {
+                    'form': UserCreationForm,
+                    'error': 'User already exist'
+                })
+
+        return render(request, 'registrarse.html', {
+            'form': UserCreationForm,
+            'error': 'Password do not match'
+        })
+
+
+def cerrarSesion(request):
+    logout(request)
+    return redirect('iniciar sesion')
